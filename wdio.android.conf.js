9545d7a4f4d98ac7340ae497ffc163451a331df9
Ademola Bhadmus <sanguine@Ademolas-MacBook-Pro.local>
2024-01-27 22:17:23 +0000

    added reporting

--- wdio.android.conf.js
+++ wdio.android.conf.js
@@ -137,7 +137,11 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec'],
+    reporters: [['allure', {
+        outputDir: 'allure-results',
+        disableWebdriverStepsReporting: true,
+        disableWebdriverScreenshotsReporting: true,
+    }]],
 
 
     
